1978b7eba0148688aa66cd8f6d827beccc81d4d1
sejin <w_star25@naver.com>
2024-09-04 18:38:47 +0900

    style: default layout padding 수정

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 function DefaultLayout() {
   return (
     <div className="flex justify-center items-center w-screen h-screen bg-gray-600">
-      <div className="w-[375px] h-full bg-white">
+      <div className="w-[375px] h-full p-4 bg-white">
         <Outlet />
       </div>
     </div>
