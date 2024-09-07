1d15c01f19054762c7410549f88727a0608f132c
정찬영 <q135794268020@gmail.com>
2024-09-08 02:58:35 +0900

    feat: KeyWord 컴포넌트 UI구현 -#22

--- src/components/contents/chat/chatwindow/keyword/KeyWordButton.tsx
+++ src/components/contents/chat/chatwindow/keyword/KeyWordButton.tsx
@@ -1,4 +1,18 @@
-const KeyWordButton = () => {
-  return <div>KeyWordButton</div>;
+interface IKeywordProps {
+  text: string;
+}
+
+const KeyWordButton = ({ text }: IKeywordProps) => {
+  // onClick시 button UI 바뀌는 것 어떻게 구현?
+  return (
+    <>
+      <button
+        className='px-4 py-2 text border rounded-full'
+        onClick={() => {}}
+      >
+        {text}
+      </button>
+    </>
+  );
 };
 export default KeyWordButton;
