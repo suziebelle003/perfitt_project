1b4d8f6e82a9c37f58d3038b7b036f836d2d82e1
정찬영 <q135794268020@gmail.com>
2024-09-08 02:57:58 +0900

    feat: 커스텀 boxShadow 추가 -#22

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'shadow-top': '0 -4px 4px rgba(0, 0, 0, 0.12)',
+      },
+    },
   },
   plugins: [],
 };
