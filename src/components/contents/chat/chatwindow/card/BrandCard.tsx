86268712ee36e59146dad39d11176ecdf12aba5a
정찬영 <q135794268020@gmail.com>
2024-09-08 13:19:18 +0900

    feat: BrandCard, 더보기 button UI 구현 -#22

--- src/components/contents/chat/chatwindow/card/BrandCard.tsx
+++ src/components/contents/chat/chatwindow/card/BrandCard.tsx
@@ -1,4 +1,27 @@
+import { brand, rightArrowShort } from '../../../../../assets/images/images';
+
 const BrandCard = () => {
-  return <div>BrandCard</div>;
+  return (
+    <>
+      <div className='border border-[#E4E4E7] rounded-md flex flex-col items-center w-fit cursor-pointer'>
+        <div className='flex items-center justify-center w-[110px] h-[103px] rounded-t-md'>
+          <img
+            src={brand}
+            alt='brand'
+          />
+        </div>
+        <div className='flex justify-center items-center relative w-full h-[29px] text-center border-t border-[#E4E4E7]'>
+          <button className='flex items-center font-medium text-[#71717A] text-[10px]'>
+            전체 상품 보기
+            <img
+              className='absolute right-4'
+              src={rightArrowShort}
+              alt='rightArrow'
+            />
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
 export default BrandCard;
