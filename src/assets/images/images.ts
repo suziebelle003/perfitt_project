a07257c0e1754f0ced29c35ffec8f776804d4e20
정찬영 <q135794268020@gmail.com>
2024-09-08 13:19:59 +0900

    feat: CardContainer UI 구현 -#22

--- src/assets/images/images.ts
+++ src/assets/images/images.ts
@@ -4,4 +4,21 @@ import polygonIcon from './polygon-icon.svg';
 import shoeSearch from './shoe-search.svg';
 import userIcon from './user-icon.svg';
 import galleryIcon from './gallery-Icon.svg';
-export { plusCircleIcon, plusIcon, polygonIcon, shoeSearch, userIcon, galleryIcon };
+import brand from './brand.svg';
+import rightArrowShort from './right-arrow-short.svg';
+import rightArrowLong from './right-arrow-long.svg';
+import exportIcon from './export-icon.svg';
+import thumbsDown from './thumbs-down.svg';
+export {
+  plusCircleIcon,
+  plusIcon,
+  polygonIcon,
+  shoeSearch,
+  userIcon,
+  galleryIcon,
+  brand,
+  rightArrowShort,
+  rightArrowLong,
+  exportIcon,
+  thumbsDown,
+};
--- src/components/contents/chat/chatwindow/card/CardContainer.tsx
+++ src/components/contents/chat/chatwindow/card/CardContainer.tsx
@@ -1,4 +1,38 @@
+import { exportIcon, thumbsDown } from '../../../../../assets/images/images';
+import BrandCard from './BrandCard';
+import MoreButton from './MoreButton';
+
+const dummyData = [1, 2, 3, 4, 5];
+
 const CardContainer = () => {
-  return <div>CardContainer</div>;
+  return (
+    <>
+      <div>
+        <div className='flex gap-2 overflow-x-auto pb-3 '>
+          {dummyData.map(() => (
+            <BrandCard />
+          ))}
+
+          <MoreButton />
+        </div>
+        <div>
+          <div className='flex px-0.2 gap-3 pb-3'>
+            <button>
+              <img
+                src={exportIcon}
+                alt='export-Icon'
+              />
+            </button>
+            <button>
+              <img
+                src={thumbsDown}
+                alt='thumbsDown'
+              />
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 export default CardContainer;
