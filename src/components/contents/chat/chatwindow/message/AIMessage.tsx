4846d3dc8aeabe92b4d558b9fc867344918beb8f
정찬영 <q135794268020@gmail.com>
2024-09-09 23:53:17 +0900

    feat: AIMessage 컴포넌트 UI 구현 -#22

---
+++ src/components/contents/chat/chatwindow/message/AIMessage.tsx
@@ -0,0 +1,25 @@
+import { perfittLogo } from '../../../../../assets/images/images';
+
+const AIMessage = () => {
+  return (
+    <>
+      <div className='px-4 flex text-sm leading-[22px]'>
+        <div className='w-7 h-7'>
+          <img
+            src={perfittLogo}
+            alt='perfitt-logo'
+          />
+        </div>
+        <div className='pl-3 py-2 pr-5 w-full'>
+          <span> OO님, 가입을 환영합니다!</span>
+          <br />
+          <span>
+            선택하신 키워드에 따라 OO님께 맞춤형 상품을 <br />
+            추천해드립니다! 관심 있는 키워드를 골라주세요.
+          </span>
+        </div>
+      </div>
+    </>
+  );
+};
+export default AIMessage;
