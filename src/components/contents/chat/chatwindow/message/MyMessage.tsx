6c663968a489f56578e7254def3441c20f20becf
정찬영 <q135794268020@gmail.com>
2024-09-09 23:52:34 +0900

    feat: MyMessage 컴포넌트 UI 구현 -#22

---
+++ src/components/contents/chat/chatwindow/message/MyMessage.tsx
@@ -0,0 +1,15 @@
+const MyMessage = () => {
+  return (
+    <>
+      <div className='flex justify-end px-4 py-5'>
+        <div className='bg-black rounded-md rounded-tr-none p-3 max-w-max '>
+          <span className='text-white text-[14px] leading-[16.71px] font-normal text-right'>
+            벤시몽 레인부츠로 가장 인기있는 제품 알려줘
+          </span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MyMessage;
