96750838ff1025b754dd5e057f6c4913e2b16cbb
ehopaak <psj9258@gmail.com>
2024-09-10 12:27:15 +0900

    feat: 좋아요 폴더구조 변경, 폰트수정, 이미지 경로 수정, 폰트 크기 수정

--- src/components/Like/LikeItems.tsx
+++ src/components/Like/LikeItems.tsx
@@ -1,3 +1,4 @@
+import aiRecomend from '../../assets/images/airecomend.svg';
 import ProductCard from '../common/ProductCard';
 import aiRecomend from '../../assets/images/airecomend.svg';
 const LikeItems = () => {
@@ -8,7 +9,7 @@ const LikeItems = () => {
         <section className='flex flex-col'>
           <div className='leading-2.5 w-[166px] h-[89px]  px-1.5 py-2.5 gap-2.5'>
             <p className='mb-[3px]'>Hoka</p>
-            <p className='mb-[10px] font-semibold  whitespace-nowrap'>호카 카하2 로우 고어텍스</p>
+            <p className='mb-[10px] font-semibold'>호카 카하2 로우 고어텍스</p>
             <p className='m-0 font-semibold'>100,000원</p>
           </div>
           {/* 이 신발 더 알아보기 버튼 */}
