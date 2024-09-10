ffa8509ea53b9865747dfa15786a87d7871bdae9
sejin <w_star25@naver.com>
2024-09-10 20:30:44 +0900

    feat: 신발 등록 페이지 UI 구현 #41

--- src/assets/images/images.ts
+++ src/assets/images/images.ts
@@ -2,6 +2,8 @@ import plusCircleIcon from "./plus-circle-icon.svg";
 import plusIcon from "./plus-icon.svg";
 import polygonIcon from "./polygon-icon.svg";
 import shoeSearch from "./shoe-search.svg";
+import starIcon from "./star-icon.svg";
+import starFillIcon from "./star-fill-icon.svg";
 import userIcon from "./user-icon.svg";
 
-export {plusCircleIcon, plusIcon, polygonIcon, shoeSearch, userIcon};
+export {plusCircleIcon, plusIcon, polygonIcon, shoeSearch, starIcon, starFillIcon, userIcon};
