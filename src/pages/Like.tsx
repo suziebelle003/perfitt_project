096fdd10bb61a4d87f4c4dbfebc92f17a93b001b
ehopaak <psj9258@gmail.com>
2024-09-14 22:15:59 +0900

    feat: like props 작업, animation추가

--- src/pages/Like.tsx
+++ src/pages/Like.tsx
@@ -1,22 +1,28 @@
-import LikeBrand from '../components/Like/LikeBrand';
-import LikeButton from '../components/Like/LikeButton';
-import LikeHeader from '../components/Like/LikeHeader';
-import LikeItems from '../components/Like/LikeItems';
+import FilterButton from '../components/common/FilterButton';
+import LikeBrand from '../components/like/LikeBrand';
+import LikeHeader from '../components/like/LikeHeader';
+import LikeItems from '../components/like/LikeItems';
+import nike from '../assets/images/nike.svg';
 
 const Like = () => {
+  const filter = ['상품', '브랜드'];
   return (
     <>
       {/* 뒤로가기 */}
       {/* header 좋아요 / 최근본  */}
       <LikeHeader />
-      {/* 좋아요 / 브랜드 튼 */}
-      <LikeButton />
-      <br></br>
+      {/* 좋아요 / 브랜드  */}
+      <FilterButton arr={filter} />
       {/* 갯수표시 */}
-      <p className='font-extrabold text-sm/[22px] mb-[15px]'>총 5개</p>
-      <LikeBrand />
+      <p className='font-extrabold text-sm/[22px] my-[15px]'>총 5개</p>
+      <LikeBrand
+        image={nike}
+        brand='NIKE'
+      >
+        나이키
+      </LikeBrand>
       {/* <div>
-        <LikeItems />
+        <LikeItems>호카 카하2 로우 고어텍스</LikeItems>
       </div> */}
     </>
   );
