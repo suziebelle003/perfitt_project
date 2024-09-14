ce1b98121fce5160dfd7d4d55d05b30ca70c95c0
ehopaak <psj9258@gmail.com>
2024-09-14 22:14:26 +0900

    feat: filter공통컴포넌트로 뺌, props작업 완료

--- src/components/plp/FilterDetail.tsx
+++ src/components/plp/FilterDetail.tsx
@@ -1,13 +1,14 @@
-import FilterButton from './FilterButton';
 import Button from '../common/Button';
+import FilterButton from '../common/FilterButton';
 
 const FilterDetail = () => {
+  const filters = ['ALL', 'WOMEN', 'MEN', 'KIDS'];
   return (
     <>
       <div className='flex flex-col gap-5'>
         <div className='flex flex-col gap-2.5'>
           <p className='font-semibold text-[15px] leading-5'>성별</p>
-          <FilterButton />
+          <FilterButton arr={filters} />
         </div>
         <Button>???개의 상품보기</Button>
       </div>
