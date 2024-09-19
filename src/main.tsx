bc1ea9826eac5b968dec04107fae10981a2db3af
sejin <w_star25@naver.com>
2024-09-19 13:43:06 +0900

    feat: 공통 컴포넌트 기능 수정

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import { RouterProvider } from 'react-router-dom';
 import router from './router/index.tsx';
 import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import AuthProvider from './service/AuthProvider.tsx';
 
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
