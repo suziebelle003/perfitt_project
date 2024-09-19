a05f283857c34dbf9e8eca406d2aeb90748ef756
정찬영 <q135794268020@gmail.com>
2024-09-20 04:36:59 +0900

    ChatWindow에 SignIn 임시 반영

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,23 +1,55 @@
 import { useContext } from 'react';
 import { AuthContext } from '../service/AuthContext';
 import SignInSocialBtn from '../components/contents/signin/SignInSocialBtn';
+import SignInAuthList from '../components/contents/signin/SignInAuthList';
+import SignInAutoKeyword from '../components/contents/signin/SignInAutoKeyword';
+import { perfittCircleLogo } from '../assets/images/images';
 
 const SignIn = () => {
   const userInfo = useContext(AuthContext);
-  console.log(userInfo);
-
+  console.log('userInfo: ', userInfo);
+  // chatwindow 컴포넌트와 header 컴포넌트 부분
   return (
     <>
       {userInfo ? (
-        <div>
-          <p>로그인한 사용자: {userInfo.email}</p>
+        <div className='flex mt-[25px]'>
+          <div className='w-20 h-20'>
+            <img
+              src={perfittCircleLogo}
+              alt='perfitt-logo'
+            />
+          </div>
+          <div className='ml-2'>
+            <p className='px-2.5 py-[5px] text-sm/[22px] break-words'>
+              반갑습니다 {userInfo.displayName == null ? userInfo.email?.split('@')[0] : userInfo.displayName}
+              님!
+              <br />
+              님을 위한 맞춤 상품을 추천해 드릴께요
+            </p>
+
+            <SignInAuthList />
+            <SignInAutoKeyword />
+          </div>
         </div>
       ) : (
-        <div>
-          <p>로그인 정보가 없습니다.</p>
+        <div className='flex mt-[25px]'>
+          <div className='w-7 h-7'>
+            <img
+              src={perfittCircleLogo}
+              alt='perfitt-logo'
+            />
+          </div>
+          <div className='ml-2'>
+            <p className='px-2.5 py-[px] text-sm/[22px] break-words'>
+              안녕하세요 펄핏AI 입니다!
+              <br /> 맞춤 추천을 위해 먼저 로그인을 해주세요.
+            </p>
+            <div className='mt-2 '>
+              <SignInSocialBtn />
+            </div>
+          </div>
         </div>
       )}
-      <SignInSocialBtn />
     </>
   );
 };
