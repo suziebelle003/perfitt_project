31b296029e4ad1602ddf677492fe709be729fa50
suziebelle003 <imsubin003@gmail.com>
2024-09-24 18:50:24 +0900

    feat: modal 기능 수정

--- src/components/common/ModalBar.tsx
+++ src/components/common/ModalBar.tsx
@@ -1,41 +1,68 @@
-import bar from '../../../assets/icons/bar.svg';
+import { ReactNode, useEffect, useState } from 'react';
 
-const SUIBar = ({
-  isBarOpen,
-  closeBar,
-  children,
-}: {
+type TBottomSheet = {
   isBarOpen: boolean;
-  closeBar: () => void;
-  children: React.ReactNode;
-}) => {
+  toggleOpenBar: () => void;
+  children: ReactNode;
+};
+
+const BottomSheet = ({ isBarOpen, toggleOpenBar, children }: TBottomSheet) => {
+  const [isDrag, setIsDrag] = useState(false);
+  const [dragStartY, setDragStartY] = useState(0);
+  const [translateY, setTranslateY] = useState(0);
+
+  const handleMouseDown = (event: React.MouseEvent) => {
+    setIsDrag(true);
+    setDragStartY(event.clientY);
+  };
+
+  useEffect(() => {
+    const handleMouseMove = (event: MouseEvent) => {
+      if (isDrag) {
+        const distanceDragged = event.clientY - dragStartY;
+        setTranslateY(Math.max(0, distanceDragged));
+      }
+    };
+
+    const handleMouseUp = (event: MouseEvent) => {
+      console.log('up', translateY);
+      if (isDrag) {
+        if (event.clientY > dragStartY + 50) toggleOpenBar();
+        else setTranslateY(0);
+        setIsDrag(false);
+      }
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener('mouseup', handleMouseUp);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener('mouseup', handleMouseUp);
+    };
+  }, [isDrag]);
+
   return (
     <>
       <div
-        className='absolute top-0 left-0 w-full h-screen z-10 bg-gray-600 opacity-50'
+        className={`absolute inset-0 z-10 transition-opacity duration-300
+          ${isBarOpen ? 'bg-gray-600 opacity-50' : 'opacity-0 pointer-events-none'}`}
         onClick={e => e.stopPropagation()}
       ></div>
-      <div className='relative w-full h-screen'>
-        <div
-          className={`w-[375px] z-20 shadow-2xl rounded-t-3xl fixed bottom-0 left-1/2 transform -translate-x-1/2 bg-white transition-transform duration-300 ${
-            isBarOpen ? 'translate-y-0' : 'translate-y-full'
-          }`}
+      <div
+        className={`absolute bottom-0 left-0 w-full h-fit p-4 z-50 shadow-2xl rounded-t-3xl bg-white
+          transform transition-transform duration-300 ${isBarOpen ? 'translate-y-0' : 'translate-y-full'}`}
+      >
+        <button
+          className='flex justify-center items-center w-full py-2'
+          onMouseDown={handleMouseDown}
         >
-          <button
-            className='flex justify-center items-center w-full py-2 z-20'
-            onClick={closeBar}
-          >
-            <img
-              src={bar}
-              alt='bar'
-              className='w-8 h-1'
-            />
-          </button>
-          {children}
-        </div>
+          <hr className='border border-gray-700 w-4' />
+        </button>
+        {children}
       </div>
     </>
   );
 };
 
-export default SUIBar;
+export default BottomSheet;
