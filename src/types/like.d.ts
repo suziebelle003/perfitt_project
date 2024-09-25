65824e93a38180b25b5b31d5be3fa6c2bca3d0b4
suziebelle003 <imsubin003@gmail.com>
2024-09-25 19:28:08 +0900

    feat: Like feature complete

--- src/types/like.d.ts
+++ src/types/like.d.ts
@@ -1,12 +1,21 @@
-export interface IProductCard {
+export type TProductCard = {
   productId: string;
   image: string;
   link: string;
   modelName: string;
   brand: string;
   modelNo: string;
-}
+};
 
-export interface IProductCardProps {
+export type TProductCardProps = {
   product: IProductCard;
-}
+};
+export type TLikeBrand = {
+  brandId: string;
+  ename: string;
+  kname: string;
+  image: string;
+};
+export type TLikeBrandProps = {
+  brands: TLikeBrand;
+};
