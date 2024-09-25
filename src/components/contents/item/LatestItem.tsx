ea2600aba28fcd663a0432bb023fb88f8b3244f6
suziebelle003 <imsubin003@gmail.com>
2024-09-25 19:36:43 +0900

    refact: Like 수정

--- src/components/contents/item/LatestItem.tsx
+++ src/components/contents/item/LatestItem.tsx
@@ -1,5 +1,8 @@
-function LatestItem() {
-  return <>Latest</>;
-}
-
+const LatestItem = () => {
+  return (
+    <>
+      <h1>LatestItem Component</h1>
+    </>
+  );
+};
 export default LatestItem;
