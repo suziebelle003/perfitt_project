c76f0ada909b2e1bc1ea5d386d14f5605d0447be
sejin <w_star25@naver.com>
2024-09-30 03:04:48 +0900

    feat: my page UI 구현

---
+++ src/components/contents/mypage/MPLinkButton.tsx
@@ -0,0 +1,27 @@
+import { useNavigate } from 'react-router-dom';
+import { rightArrowIcon } from '../../../assets/icons/icons';
+
+type TMPBottomButtonProps = {
+  title: string;
+  imgAlt: string;
+  link: string;
+};
+
+function MPLinkButton({ title, imgAlt, link }: TMPBottomButtonProps) {
+  const navigate = useNavigate();
+
+  return (
+    <button
+      className='flex justify-between items-center p-4 border-b border-[#E4E4E7] hover:bg-[#F5F5F5]'
+      onClick={() => navigate(`${link}`)}
+    >
+      <div className='text-[14px] leading-[17px] font-medium'>{title}</div>
+      <img
+        src={rightArrowIcon}
+        alt={imgAlt}
+      />
+    </button>
+  );
+}
+
+export default MPLinkButton;
--- src/pages/mypage/MyPage.tsx
+++ src/pages/mypage/MyPage.tsx
@@ -1,20 +1,151 @@
-import UserInfo from '../../components/contents/mypage/UserInfo';
-import MyPageBottom from '../../components/contents/mypage/MyPageBottom';
-import UserProfile from '../../components/contents/mypage/UserProfile';
-import LikeAndShoes from '../../components/contents/mypage/LikeAndShoes';
-import HeaderLayout from '../../layout/HeaderLayout';
+// 마이 페이지
 
-const MyPage = () => {
+// 로그인 안 했을 때 로그인 link
+// 프로필 사진 변경
+// 내 정보 수정 link 변경
+// 비밀번호 변경 link 변경
+// 고객센터 link 변경
+// 회원탈퇴
+
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useUserStore } from '../../stores/user.store';
+import HeaderLayout from '../../layout/HeaderLayout';
+import MPLinkButton from '../../components/contents/mypage/MPLinkButton';
+import { cameraMiniIcon, heartLineIcon, smileIcon, userBorderIcon } from '../../assets/icons/icons';
+
+function MyPage() {
+  const uid = 'qKnJXMMf4xd8KAn9UtGqegZFyjv2'; // uid 가져오기
+
+  const navigate = useNavigate();
+  const { user, fetchUserInfo } = useUserStore();
+
+  const editUserImg = () => {};
+
+  useEffect(() => {
+    fetchUserInfo(uid);
+  });
+
   return (
     <HeaderLayout back>
-      <UserProfile />
+      <div className='h-full flex flex-col'>
+        <div className='flex-1'>
+          {/* user profile */}
+          <div className='flex flex-col justify-center items-center'>
+            <div className='relative w-20 h-20'>
+              <img
+                src={userBorderIcon}
+                alt='User profile'
+                className='w-full h-full object-cover rounded-full overflow-hidden'
+              />
+              <button
+                className='absolute bottom-0 right-0 w-5 h-5
+              flex justify-center items-center
+              rounded-full bg-white shadow-[0_0_2.67px_0_#00000040]'
+                onClick={editUserImg}
+              >
+                <img
+                  src={cameraMiniIcon}
+                  alt='Edit user image'
+                  className='w-[14px] h-[14px]'
+                />
+              </button>
+            </div>
 
-      <LikeAndShoes />
+            <div className='mt-[17px] flex flex-col items-center gap-2 mb-[20px]'>
+              <p className='text-[15px] leading-[18px]'>안녕하세요!</p>
+              <div className='text-[20px] leading-6'>
+                <span className='font-medium tracking-[0.25em]'>{user?.name}</span>
+                <span className='font-light'>님</span>
+              </div>
+            </div>
+          </div>
 
-      <UserInfo />
+          {/* like & shoerack */}
+          <div className='flex border-y-[6px] border-y-[#F5F5F5]'>
+            <button
+              className='flex-1 flex justify-center items-center gap-2.5 h-[60px] hover:bg-[#F5F5F5]'
+              onClick={() => navigate('/mypage/item/like')}
+            >
+              <img
+                src={heartLineIcon}
+                alt='Liked item'
+                className='w-[14px] h-[13px]'
+              />
+              <div className='text-[14px] font-medium'>좋아요</div>
+            </button>
+            <button
+              className='flex-1 flex justify-center items-center gap-2.5 h-[60px]
+            border-l border-[#F5F5F5] hover:bg-[#F5F5F5]'
+              onClick={() => navigate('/shoerack/main')}
+            >
+              <img
+                src={smileIcon}
+                alt='Shoe rack'
+                className='w-[14px] h-[14px]'
+              />
+              <div className='text-[14px] font-medium'>신발장</div>
+            </button>
+          </div>
 
-      <MyPageBottom />
+          {/* user info */}
+          <div
+            className='px-4 py-[25px] flex flex-col gap-[15px]
+          border-b-[6px] border-b-[#F5F5F5]
+          text-[14px] leading-[17px] font-medium'
+          >
+            내 정보
+            <div className='flex flex-col gap-4'>
+              <div className='flex gap-2.5'>
+                <div className='w-[123px] text-[#808080]'>이름</div>
+                <div className='font-normal'>{user?.name}</div>
+              </div>
+              <div className='flex gap-2.5'>
+                <div className='w-[123px] text-[#808080]'>성별</div>
+                <div className='font-normal'>{user?.gender}</div>
+              </div>
+              <div className='flex gap-2.5'>
+                <div className='w-[123px] text-[#808080]'>생년월일</div>
+                <div className='font-normal'>
+                  {user?.birth?.year}.{user?.birth?.month}.{user?.birth?.day}
+                </div>
+              </div>
+              <div className='flex gap-2.5'>
+                <div className='w-[123px] text-[#808080]'>평소사이즈</div>
+                <div className='font-normal'>{user?.size}</div>
+              </div>
+            </div>
+          </div>
+
+          <div className='flex flex-col'>
+            <MPLinkButton
+              title='내 정보 수정'
+              imgAlt='Edit my infomation'
+              link='/'
+            />
+            <MPLinkButton
+              title='비밀번호 변경'
+              imgAlt='Change password'
+              link='/'
+            />
+            <MPLinkButton
+              title='고객센터'
+              imgAlt='Customer service'
+              link='/'
+            />
+          </div>
+        </div>
+
+        {/* bottom */}
+        <div
+          className='mt-6 flex justify-center gap-9 py-[30px]
+            text-[14px] leading-[17px] text-[#808080] hover:underline'
+        >
+          <button>회원탈퇴</button>
+          {/* <button>고객약관</button> */}
+        </div>
+      </div>
     </HeaderLayout>
   );
-};
+}
 export default MyPage;
