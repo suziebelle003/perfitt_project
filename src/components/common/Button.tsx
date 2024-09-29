0349139939d7183d522249e53cd4b8fb1b01c9ca
sejin <w_star25@naver.com>
2024-09-30 06:29:26 +0900

    refactor: twMerge 제거

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,19 +1,15 @@
-import { twMerge } from 'tailwind-merge';
-
 type TButtonProps = React.ComponentPropsWithoutRef<'button'>;
 
 const Button = (props: TButtonProps) => {
   const { className, children, ...rest } = props;
 
   return (
-    <>
-      <button
-        className={twMerge('w-full h-[56px] rounded bg-black text-white text-base font-semibold', className)}
-        {...rest}
-      >
-        {children}
-      </button>
-    </>
+    <button
+      className={`${className} w-full h-[56px] rounded bg-black text-base text-white font-semibold`}
+      {...rest}
+    >
+      {children}
+    </button>
   );
 };
 
