8a3db85eef99108387a3082a3d9b004d62c7ea99
sejin <w_star25@naver.com>
2024-10-02 22:21:32 +0900

    chore: 이미지 경로 수정

--- src/assets/images/images.ts
+++ src/assets/images/images.ts
@@ -1,12 +1,9 @@
 import abcmartLogo from './abcmart-logo.png';
-import brand from './brand.svg';
 import checkIcon from './check-icon.svg';
 import downArrowIcon from './down-arrow-icon.svg';
-import exportIcon from './export-icon.svg';
 import footLoading from './foot-loading.gif';
 import footScanning from './foot-scanning.png';
 import footprint404 from './footprint-404.svg';
-import galleryIcon from './gallery-Icon.svg';
 import kolonLogo from './kolon-logo.png';
 import obList1 from './ob-list1.png';
 import obList2 from './ob-list2.png';
@@ -14,24 +11,17 @@ import perfittCircleLogo from './perfitt-circle-logo.svg';
 import perfittLogo from './perfitt-logo.svg';
 import questionMark from './question-mark.svg';
 import rightArrowLong from './right-arrow-long.svg';
-import rightArrowShort from './right-arrow-short.svg';
 import shoeSearch from './shoe-search.svg';
 import shoeSilhouette from './shoe-silhouette.png';
 import shoemarkerLogo from './shoemarker-logo.png';
-import thumbsDown from './thumbs-down.svg';
-import upArrow from './up-arrow.svg';
-import abcMart from './abcmart.svg';
 
 export {
   abcmartLogo,
-  brand,
   checkIcon,
   downArrowIcon,
-  exportIcon,
   footLoading,
   footScanning,
   footprint404,
-  galleryIcon,
   kolonLogo,
   obList1,
   obList2,
@@ -39,11 +29,7 @@ export {
   perfittLogo,
   questionMark,
   rightArrowLong,
-  rightArrowShort,
   shoeSearch,
   shoeSilhouette,
   shoemarkerLogo,
-  thumbsDown,
-  upArrow,
-  abcMart,
 };
