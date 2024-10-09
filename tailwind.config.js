535376e03874761b96285af7b5bc0dfa8f3edb33
sejin <w_star25@naver.com>
2024-10-10 08:20:26 +0900

    refactor: tailwind-merge, tailwind-scrollbar-hide 삭제, framer-motion 설치

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,29 +3,7 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      boxShadow: {
-        'shadow-top': '0 -2px 2px rgba(0, 0, 0, 0.1)',
-      },
       keyframes: {
-        'fade-in': {
-          '0%': {
-            opacity: 0,
-          },
-          '100%': {
-            opacity: 1,
-          },
-        },
-        'drop-in': {
-          '0%': {
-            opacity: '0',
-            transform: 'scale(0)',
-            animationTimingFunction: 'cubic-bezier(0.34, 1.61, 0.7, 1)',
-          },
-          '100%': {
-            opacity: '1',
-            transform: 'scale(1)',
-          },
-        },
         filterReset: {
           '0%': { filter: 'contrast(0) brightness(2)' },
           '100%': { filter: 'contrast(1) brightness(1)' },
@@ -37,8 +15,6 @@ export default {
         },
       },
       animation: {
-        fadeIn: 'fade-in 1s ease-in-out 0.25s 1',
-        dropIn: 'drop-in 0.5s ease-in-out 0.25s 1',
         footprint1: 'footprint 3s ease-in-out 0s 1',
         footprint2: 'footprint 3s ease-in-out 0.5s 1',
         footprint3: 'footprint 3s ease-in-out 1s 1',
@@ -49,5 +25,5 @@ export default {
       },
     },
   },
-  plugins: [require('tailwind-scrollbar-hide')],
+  plugins: [],
 };
