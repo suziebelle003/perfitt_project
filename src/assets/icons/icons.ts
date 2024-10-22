fc8c48db53fe88bf5c0126b5d7b2fe061eb09bd1
sejin <w_star25@naver.com>
2024-10-22 20:16:12 +0900

    feat: chat 관련 아이콘 추가, 불필요한 아이콘 삭제

--- src/assets/icons/icons.ts
+++ src/assets/icons/icons.ts
@@ -2,7 +2,10 @@ import aiBalloonIcon from './ai-balloon-icon.svg';
 import cameraIcon from './camera-icon.svg';
 import cameraMiniIcon from './camera-mini-icon.svg';
 import chatIcon from './chat-icon.svg';
+import checkIcon from './check-icon.svg';
+import copyIcon from './copy-icon.svg';
 import exportIcon from './export-icon.svg';
+import exportWhiteIcon from './export-white-icon.svg';
 import footprintIcon from './footprint-icon.svg';
 import galleryIcon from './gallery-Icon.svg';
 import heartFilledIcon from './heart-filled-icon.svg';
@@ -10,9 +13,12 @@ import heartIcon from './heart-icon.svg';
 import heartLineIcon from './heart-line-icon.svg';
 import infoIcon from './info-icon.svg';
 import leftArrowIcon from './left-arrow-icon.svg';
+import linkAngledIcon from './link-angled-icon.svg';
+import loadingIcon from './loading-icon.svg';
 import menuIcon from './menu-icon.svg';
 import pencilIcon from './pencil-icon.svg';
 import pencilRedIcon from './pencil-red-icon.svg';
+import planetIcon from './planet-icon.svg';
 import plusCircleIcon from './plus-circle-icon.svg';
 import plusIcon from './plus-icon.svg';
 import plusMiniIcon from './plus-mini-icon.svg';
@@ -26,6 +32,7 @@ import starIcon from './star-icon.svg';
 import thumbsDownIcon from './thumbs-down-icon.svg';
 import trashIcon from './trash-icon.svg';
 import trashRedIcon from './trash-red-icon.svg';
+import trashWhiteIcon from './trash-white-icon.svg';
 import upArrowIcon from './up-arrow-icon.svg';
 import userBorderIcon from './user-border-icon.svg';
 import userIcon from './user-icon.svg';
@@ -37,7 +44,10 @@ export {
   cameraIcon,
   cameraMiniIcon,
   chatIcon,
+  checkIcon,
+  copyIcon,
   exportIcon,
+  exportWhiteIcon,
   footprintIcon,
   galleryIcon,
   heartFilledIcon,
@@ -45,9 +55,12 @@ export {
   heartLineIcon,
   infoIcon,
   leftArrowIcon,
+  linkAngledIcon,
+  loadingIcon,
   menuIcon,
   pencilIcon,
   pencilRedIcon,
+  planetIcon,
   plusCircleIcon,
   plusIcon,
   plusMiniIcon,
@@ -61,6 +74,7 @@ export {
   thumbsDownIcon,
   trashIcon,
   trashRedIcon,
+  trashWhiteIcon,
   upArrowIcon,
   userBorderIcon,
   userIcon,
