c2a1696989135446556adb689996e48eb2b2e606
suziebelle003 <imsubin003@gmail.com>
2024-10-30 19:01:50 +0900

    fix : 오류 수정

--- src/api/firebase/createUser.ts
+++ src/api/firebase/createUser.ts
@@ -1,16 +1,16 @@
-import { doc, setDoc } from "firebase/firestore";
-import { db } from "../../service/firebase";
-import { TUserInfo } from "../../types/sign";
+import { doc, setDoc } from 'firebase/firestore';
+import { db } from '../../service/firebase';
+import { TUserInfo } from '../../types/sign';
 
 export const createUser = async (uid: string, user: TUserInfo) => {
   try {
-    const { password, birthYear, birthMonth, birthDay, ...rest } = user;
+    const { password, year, month, day, ...rest } = user;
     await setDoc(doc(db, 'user', uid), {
       ...rest,
       birth: {
-        year: parseInt(birthYear.slice(0, -1)),
-        month: parseInt(birthMonth.slice(0, -1)),
-        day: parseInt(birthDay.slice(0, -1)),
+        year: year ? parseInt(year.toString().slice(0, -1)) : null,
+        month: month ? parseInt(month.toString().slice(0, -1)) : null,
+        day: day ? parseInt(day.toString().slice(0, -1)) : null,
       },
     });
 
--- src/types/like.d.ts
+++ src/types/like.d.ts
@@ -1,10 +1,11 @@
-import { TProduct } from "./db";
+import { TProduct } from './db';
 
 export type TLikeProduct = {
   like: boolean;
+  datetime?: string;
 } & TProduct;
 
 export type TPartner = {
   name: string;
   image: string;
-}
+};
